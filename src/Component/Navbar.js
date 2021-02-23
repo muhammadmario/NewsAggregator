9dbf0c8e0cb04aeeefa8253e5938634003583b90
muhammadmario <acernitro58@gmail.com>
2021-02-23 18:11:50 +0700

    finish design navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Navbar.css";
 
 function Navbar() {
-  return <div className="navbar"></div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <i class="far fa-newspaper"></i>
+        <p>NEWS</p>
+      </div>
+      <div className="mode">
+        <i class="far fa-sun"></i>
+        <p>Mode</p>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
